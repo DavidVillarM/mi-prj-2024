b1e0b4e794d3a3e2c00ae8fc55a5e263a826215d
DaSViM <david.villar@fiuni.edu.py>
2024-09-16 23:47:46 -0400

    primera prueba vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MiPrueba mensaje/>
+  <ListaComida/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MiPrueba from './components/MiPrueba.vue'
+import ListaComida from './components/ListaComida.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    MiPrueba,
+    ListaComida
   }
 }
 </script>
@@ -22,5 +28,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
---
+++ src/components/MiPrueba.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ mensaje }}</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'MiPrueba',
+        data(){
+            return{
+                mensaje: 'Hola desde el componente!'
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    h1{
+        color: green;
+    }
+</style>
